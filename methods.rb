61a2a5ce69db4f671fceead6cedeeb121ba77deb
NicMortelliti <npmortelliti@gmail.com>
2022-05-23 15:38:09 -0700

    Run my first Ruby method!

--- methods.rb
+++ methods.rb
@@ -1,1 +1,11 @@
-# Your code here!
+# 'def' identifies this code as a method
+# Write the name of the method in snake-case
+def my_method(param)
+  puts "Running my_method"
+  
+  # Last line in the method is implicitly the return value.
+  # 'return' can be used, but commonly isn't.
+  param + 1
+
+# 'end' identifies the end of the method (like JS closing curly bracket)   
+end
